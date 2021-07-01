b649278da137786ab3056679a9ebe2beca464415
doggai10 <doggai77@gmail.com>
2021-07-01 17:34:09 +0900

    update: jpa interface 추가

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -2,18 +2,24 @@ package study.datajpa.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@ToString(of={"id", "username", "age"})
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name= "member_id")
     private Long id;
     private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="team_id")
+    private Team team;
 
     protected Member() {
     }
@@ -22,7 +28,21 @@ public class Member {
         this.username = username;
     }
 
+    public Member(String username, int age, Team team) {
+        this.username=username;
+        this.age=age;
+        if(team!=null){
+            changeTeam(team);
+        }
+    }
+
+
     public void changeUserName(String username){
         this.username=username;
+    }
+
+    public void changeTeam(Team team){
+        this.team=team;
+        team.getMembers().add(this);
     }
 }
